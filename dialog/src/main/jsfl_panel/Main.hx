491138894fef824f21d764239bb84af4d3e5226b
siratama <webmaster@dango-itimi.com>
2014-02-04 18:53:52 +0900

    reformat

--- dialog/src/main/jsfl_panel/Main.hx
+++ dialog/src/main/jsfl_panel/Main.hx
@@ -11,9 +11,9 @@ class Main {
 	private var flashFileDirectory:String;
 
 	private static inline var TEXT_KEY_FLASH_EXTERN = "FLASH_EXTERN";
-    private static inline var TEXT_KEY_FLASH = "FLASH";
+	private static inline var TEXT_KEY_FLASH = "FLASH";
 	private static inline var TEXT_KEY_CREATEJS = "CREATEJS";
-    private static inline var TEXT_KEY_OPENFL = "OPENFL";
+	private static inline var TEXT_KEY_OPENFL = "OPENFL";
 	private static inline var TEXT_KEY_JS_NAMESPACE = "JS";
 
 	public static function main(){
@@ -36,21 +36,21 @@ class Main {
 		var path:String = executeJsflCommand("fl.getDocumentDOM().pathURI;");
 		var arr = path.split("/");
 		arr.pop();
-		flashFileDirectory =  arr.join("/") + "/";
+		flashFileDirectory = arr.join("/") + "/";
 	}
 	private function setDefaultText(){
 
-        setDefaultTextCommon(TEXT_KEY_FLASH_EXTERN, view.flashExternOutputPathText);
-        setDefaultTextCommon(TEXT_KEY_FLASH, view.flashOutputPathText);
-        setDefaultTextCommon(TEXT_KEY_CREATEJS, view.createJsOutputPathText);
-        setDefaultTextCommon(TEXT_KEY_OPENFL, view.openflOutputPathText);
-        setDefaultTextCommon(TEXT_KEY_JS_NAMESPACE, view.jsSymbolNamespaceText);
+		setDefaultTextCommon(TEXT_KEY_FLASH_EXTERN, view.flashExternOutputPathText);
+		setDefaultTextCommon(TEXT_KEY_FLASH, view.flashOutputPathText);
+		setDefaultTextCommon(TEXT_KEY_CREATEJS, view.createJsOutputPathText);
+		setDefaultTextCommon(TEXT_KEY_OPENFL, view.openflOutputPathText);
+		setDefaultTextCommon(TEXT_KEY_JS_NAMESPACE, view.jsSymbolNamespaceText);
 	}
-    private function setDefaultTextCommon(textKey:String, textField:TextField){
+	private function setDefaultTextCommon(textKey:String, textField:TextField){
 
-        var text = getDataFromDocument(textKey);
-        if(text != "0") textField.text = text;
-    }
+		var text = getDataFromDocument(textKey);
+		if(text != "0") textField.text = text;
+	}
 	private function onMouseDownClose(event){
 		closeDialog();
 	}
@@ -60,30 +60,28 @@ class Main {
 		executeJsflCommand('fl.runScript("$jsflUri");');
 
 		var flashExternDirectory = view.flashExternOutputPathText.text;
-        var flashDirectory = view.flashOutputPathText.text;
+		var flashDirectory = view.flashOutputPathText.text;
 		var createJsDirectory = view.createJsOutputPathText.text;
-        var openflDirectory = view.openflOutputPathText.text;
+		var openflDirectory = view.openflOutputPathText.text;
 
 		var jsSymbolNamespace = view.jsSymbolNamespaceText.text;
 		executeJsflCommand('new Main("$flashFileDirectory", "$flashExternDirectory", "$flashDirectory", "$createJsDirectory", "$openflDirectory", "$jsSymbolNamespace");');
 		closeDialog();
 
 		addDataToDocument(TEXT_KEY_FLASH_EXTERN, flashExternDirectory);
-        addDataToDocument(TEXT_KEY_FLASH, flashDirectory);
+		addDataToDocument(TEXT_KEY_FLASH, flashDirectory);
 		addDataToDocument(TEXT_KEY_CREATEJS, createJsDirectory);
-        addDataToDocument(TEXT_KEY_OPENFL, openflDirectory);
+		addDataToDocument(TEXT_KEY_OPENFL, openflDirectory);
 		addDataToDocument(TEXT_KEY_JS_NAMESPACE, jsSymbolNamespace);
 	}
-	private function executeJsflCommand(command:String):Dynamic{
 
+	private function executeJsflCommand(command:String):Dynamic{
 		return untyped __global__["adobe.utils.MMExecute"](command);
 	}
 	private function flTrace(text:String){
-
 		executeJsflCommand('fl.trace("$text");');
 	}
 	private function closeDialog(){
-
 		executeJsflCommand("fl.xmlui.cancel();");
 	}
 	private function addDataToDocument(key:String, data:String){
